d256eeb8c867d81c5cb17624d6abf3c08bd88bf8
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-10-20 21:14:41 +0200

    forecast

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import './App.css';
 import CurrentWeather from './components/current-weather/current-weather';
+import Forecast from './components/forecast/forecast';
 import Search from './components/search/search';
 
 function App() {
@@ -31,6 +32,7 @@ console.log(forecast)
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
       {currentWeather && <CurrentWeather data={currentWeather}/>}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
